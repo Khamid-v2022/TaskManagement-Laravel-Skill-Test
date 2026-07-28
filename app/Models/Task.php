f2e927a29e2ddf2a98e2688bf7411508b31fdaf4
Khamid-v2022 <khamid.webdev@gmail.com>
2026-07-28 13:32:16 +0300

    Remove created_by field - no need auth..

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['name', 'priority', 'status', 'description', 'created_by'];
-    
-    public function createdBy()
-    {
-        return $this->belongsTo(User::class, 'created_by');
-    }
+    protected $fillable = ['name', 'priority', 'status', 'description'];
 }
--- database/migrations/2026_07_28_101954_create_tasks_table.php
+++ database/migrations/2026_07_28_101954_create_tasks_table.php
@@ -18,8 +18,7 @@ return new class extends Migration
 
             $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
             $table->text('description')->nullable();
-            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
-            
+
             $table->timestamps();
         });
     }
